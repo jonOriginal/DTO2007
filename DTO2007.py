98043e09e721add33f2a395d1b34ff7078d6367d
jonOriginal <79869389+jonOriginal@users.noreply.github.com>
2022-07-28 20:37:43 +1200

    Finished Page 1 field validation, error handling, button validation

--- DTO2007.py
+++ DTO2007.py
@@ -7,10 +7,8 @@ DTO2007Y2A V1
 import tkinter as tk
 from tkinter import ttk
 
-
 from tkinter import messagebox
-
-from pyrsistent import v
+from tkinter.tix import *
 
 class App(tk.Tk):
     def __init__(self, *args, **kwargs):
@@ -37,38 +35,45 @@ class App(tk.Tk):
 
 class StartPage(tk.Frame):
     def __init__(self,parent,container):
-        self.temp_dimensions = parent.temp_dimensions
         super().__init__(container)
+        self.temp_dimensions = parent.temp_dimensions
+        self.error_container = {"height":'',"width":'',"length":''}
+        
         reg=self.register(self.callback)
+    
         for i,fields, in enumerate(self.temp_dimensions):
-            labs = ttk.Label(self,text=fields)
-            ents = ttk.Entry(self)
-            ents.config(validate='all', validatecommand=(reg, '%P','%V'),textvariable=self.temp_dimensions[fields])
-            labs.grid(row=i,column=0)
-            ents.grid(row=i,column=1)
-            self.temp_dimensions[fields] = ents
+            labs = ttk.Label(self,text=fields,)
+            self.ents = ttk.Entry(self)
+            errs = ttk.Label(self,text='',foreground='red')
+
+            self.ents.config(validate='all', validatecommand=(reg, '%P','%V',fields),textvariable=self.temp_dimensions[fields])
+
+            errs.grid(row=i,column=2,sticky='w',padx=5,pady=5)
+            labs.grid(row=i,column=0,padx=5, pady=5)
+            self.ents.grid(row=i,column=1,padx=5, pady=5)
+
+            self.error_container[fields] = errs
+            self.temp_dimensions[fields] = self.ents
+            
         self.error_label = ttk.Label(self,text='')
         self.error_label.grid(row=i+2,column=0)
+        
         next_button = ttk.Button(self,text="Next",command=lambda:self.confirm_next(parent))
-        next_button.grid(row=i+1,column=0)
+        next_button.grid(row=i+1,column=0,padx=5, pady=5)
 
-    def callback(self,value,reason):
-        print(value,reason)
+    def callback(self,value,reason,name):
         try:
             float(value)
             if reason == 'focusout':
-                if float(value) > 100:
-                    self.error_label.config(text='Max 100')
-                elif float(value) < 5:
-                    self.error_label.config(text='Min 5')
+                if float(value) < 5 or float(value) > 100:
+                    self.temp_dimensions[name].config(foreground='red')
+                    self.error_container[name].config(text='Please enter a value between 5 and 100')
                 else:
-                    self.error_label.config(text='')
-                return True
-            if reason == 'key':
-                self.error_label.config(text='')
-                return True
+                    self.temp_dimensions[name].config(foreground='black')
+                    self.error_container[name].config(text='')
+            return True
         except:
-            if input == '':
+            if value == '':
                 return True
             else:
                 return False
@@ -77,6 +82,8 @@ class StartPage(tk.Frame):
         values = [self.temp_dimensions['height'].get(),self.temp_dimensions['width'].get(),self.temp_dimensions['length'].get()]
         if all(values) == False:
             messagebox.showerror('Empty Fields', 'Please Fill all fields')
+        elif all(map(lambda x: float(x) > 5 and float(x) < 100,values)) == False:
+            messagebox.showerror('Invalid Fields', 'Please enter a value between 5 and 100')
         else:
             parent.switch_frame(Page_2)
 
@@ -88,9 +95,9 @@ class Page_2(tk.Frame):
         
         for i, radios in enumerate(self.region_multiplier):
             self.rad = ttk.Radiobutton(self,text=radios,value=self.region_multiplier[radios],variable=self.region_var)
-            self.rad.grid(column=0,row=i)
+            self.rad.grid(column=0,row=i,padx=5,pady=5,sticky='w')
         region_confirm = ttk.Button(self,text='confirm',command=lambda:self.confirm_next(parent))
-        region_confirm.grid(column=0,row=i+1)
+        region_confirm.grid(column=0,row=i+1,padx=5,pady=5,sticky='w')
         
     def confirm_next(self,parent):
         if self.region_var.get() == '':
